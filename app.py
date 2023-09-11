578664899ecd70af7cd92370de304c3638ace8ea
Pranav <pranav.techiegeek@gmail.com>
2023-09-11 12:59:31 +0530

    lab ready

---
+++ app.py
@@ -0,0 +1,16 @@
+items = {
+    "milk": 2.6,
+    "bread": 1.4,
+    "egg": 5
+}
+
+lst = ["milk", "bread", "Egg"]
+total = 0
+
+for ele in lst:
+    print(ele, items[ele])
+    total += items[ele]
+
+print('-'*10)
+print('Total:', total)
+print('-'*10)
